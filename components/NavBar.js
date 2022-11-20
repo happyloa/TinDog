e1fedd8e944baba992f54cd3e8b2c8fe3d8ac4f6
Wolfpack <piafyoyo06@gmail.com>
2022-11-21 05:06:15 +0800

    feat: 更新導航列

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,37 +1,39 @@
 export default function NavBar() {
   return (
-    <nav className="navbar bg-dark navbar-expand-lg navbar-dark">
-      <a className="navbar-brand" href="">
-        Tindog
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav ms-auto">
-          <li className="nav-item">
-            <a className="nav-link" href="">
-              Contact
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="">
-              Pricing
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="">
-              Download
-            </a>
-          </li>
-        </ul>
+    <nav class="navbar navbar-dark navbar-expand-lg bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          Tindog
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Contact
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Pricing
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Download
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
