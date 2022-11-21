d9a7b980f0c9ca32452a2087da031fb1f33b7ce2
Wolfpack <piafyoyo06@gmail.com>
2022-11-21 09:41:54 +0800

    feat: 引入 Styled Components 套件並增加對應的元素的樣式

--- components/Title.js
+++ components/Title.js
@@ -1,14 +1,30 @@
+// 引入 Styled Components 套件
+import styled from "styled-components";
+
+const TitleSection = styled.section`
+  background-color: #ff4c68;
+`;
+
+const TitleH1 = styled.h1`
+  font-family: "Montserrat", sans-serif;
+  font-weight: 900;
+  font-size: 3rem;
+  line-height: 1.5;
+`;
+
 export default function Title(props) {
   return (
-    <section id="title">
-      <div>
-        <h1>{props.headingText}</h1>
-        <button type="button">{props.cta}</button>
-        <button type="button">{props.cta}</button>
+    <TitleSection id="title">
+      <div className="row">
+        <div className="col-12 col-md-6">
+          <TitleH1>{props.headingText}</TitleH1>
+          <button type="button">{props.cta}</button>
+          <button type="button">{props.cta}</button>
+        </div>
+        <div className="col-12 col-md-6">
+          <img src="/iphone6.png" alt="iphone-mockup" />
+        </div>
       </div>
-      <div>
-        <img src="/iphone6.png" alt="iphone-mockup" />
-      </div>
-    </section>
+    </TitleSection>
   );
 }
