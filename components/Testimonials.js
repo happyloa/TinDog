cfe75dbbedd988896f7c2da8eefd1462ba503565
Tsung-Yu, Hsieh <piafyoyo06@gmail.com>
2022-11-22 17:22:40 +0800

    feat: 新增見證樣式

--- components/Testimonials.js
+++ components/Testimonials.js
@@ -1,16 +1,37 @@
+// 引入 Styled Components 套件
+import styled from "styled-components";
+
+const StyledSection = styled.section`
+  background-color: #ef8172;
+  padding: 7% 15%;
+`;
+
+const TitleH2 = styled.h2`
+  font-size: 3rem;
+`;
+
+const TestimonialsImg = styled.img`
+  width: 10%;
+  margin: 20px;
+`;
+
 export default function Testimonials() {
   return (
-    <section id="testimonials">
-      <h2>
+    <StyledSection id="testimonials" className="text-center text-white">
+      <TitleH2 className="fw-bold lh-base">
         I no longer have to sniff other dogs for love. I&apos;ve found the
         hottest Corgi on TinDog. Woof.
-      </h2>
-      <img src="/dog-img.jpg" alt="dog-profile" />
+      </TitleH2>
+      <TestimonialsImg
+        src="/dog-img.jpg"
+        alt="dog-profile"
+        className="rounded-circle"
+      />
       <em>Pebbles, New York</em>
 
-      {/* <h2 class="testimonial-text">My dog used to be so lonely, but with TinDog's help, they've found the love of their life. I think.</h2>
+      {/* <TitleH2 class="testimonial-text">My dog used to be so lonely, but with TinDog's help, they've found the love of their life. I think.</TitleH2>
     <img class="testimonial-image" src="/lady-img.jpg" alt="lady-profile"/>
     <em>Beverly, Illinois</em> */}
-    </section>
+    </StyledSection>
   );
 }
