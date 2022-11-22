6a6859e56bf9becb1329aaa979ef31b35dbce9ac
Tsung-Yu, Hsieh <piafyoyo06@gmail.com>
2022-11-22 14:19:23 +0800

    feat: 新增媒體 Logo 元件的樣式

--- components/Press.js
+++ components/Press.js
@@ -1,10 +1,24 @@
+// 引入 Styled Components 套件
+import styled from "styled-components";
+
+const StyledSection = styled.section`
+  background-color: #ef8172;
+  text-align: center;
+  padding-bottom: 3%;
+`;
+
+const PressLogos = styled.img`
+  width: 15%;
+  margin: 20px 20px 50px;
+`;
+
 export default function Press() {
   return (
-    <section id="press">
-      <img src="/techcrunch.png" alt="tc-logo" />
-      <img src="/tnw.png" alt="tnw-logo" />
-      <img src="/bizinsider.png" alt="biz-insider-logo" />
-      <img src="/mashable.png" alt="mashable-logo" />
-    </section>
+    <StyledSection id="press">
+      <PressLogos src="/techcrunch.png" alt="tc-logo" />
+      <PressLogos src="/tnw.png" alt="tnw-logo" />
+      <PressLogos src="/bizinsider.png" alt="biz-insider-logo" />
+      <PressLogos src="/mashable.png" alt="mashable-logo" />
+    </StyledSection>
   );
 }
