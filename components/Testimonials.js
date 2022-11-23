ed05337340f1ece8801afcd9f97ad1c8023c2e8c
Wolfpack <piafyoyo06@gmail.com>
2022-11-24 05:37:40 +0800

    feat: 加入輪播到見證區塊中

--- components/Testimonials.js
+++ components/Testimonials.js
@@ -18,20 +18,54 @@ const TestimonialsImg = styled.img`
 export default function Testimonials() {
   return (
     <StyledSection id="testimonials" className="text-center text-white">
-      <TitleH2 className="fw-bold lh-base">
-        I no longer have to sniff other dogs for love. I&apos;ve found the
-        hottest Corgi on TinDog. Woof.
-      </TitleH2>
-      <TestimonialsImg
-        src="/images/testimonials/dog-img.webp"
-        alt="dog-profile"
-        className="rounded-circle"
-      />
-      <em>Pebbles, New York</em>
-
-      {/* <TitleH2 class="testimonial-text">My dog used to be so lonely, but with TinDog's help, they've found the love of their life. I think.</TitleH2>
-    <TestimonialsImg class="testimonial-image" src="/lady-img.webp" alt="lady-profile"/>
-    <em>Beverly, Illinois</em> */}
+      <div
+        id="testimonialCarousel"
+        className="carousel slide"
+        data-bs-ride="true"
+        data-bs-pause="hover">
+        <div className="carousel-inner">
+          {/* 第一個見證 */}
+          <div className="carousel-item active">
+            <TitleH2 className="fw-bold lh-base">
+              I no longer have to sniff other dogs for love. I&apos;ve found the
+              hottest Corgi on TinDog. Woof.
+            </TitleH2>
+            <TestimonialsImg
+              src="/images/testimonials/dog-img.webp"
+              alt="dog-profile"
+              className="rounded-circle"
+            />
+            <em>Pebbles, New York</em>
+          </div>
+          {/* 第二個見證 */}
+          <div className="carousel-item">
+            <TitleH2 className="fw-bold lh-base">
+              My dog used to be so lonely, but with TinDog&apos;s help,
+              they&apos;ve found the love of their life. I think.
+            </TitleH2>
+            <TestimonialsImg
+              src="/images/testimonials/lady-img.webp"
+              alt="lady-profile"
+              className="rounded-circle"
+            />
+            <em>Beverly, Illinois</em>
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#testimonialCarousel"
+          data-bs-slide="prev">
+          <span className="carousel-control-prev-icon"></span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#testimonialCarousel"
+          data-bs-slide="next">
+          <span className="carousel-control-next-icon"></span>
+        </button>
+      </div>
     </StyledSection>
   );
 }
