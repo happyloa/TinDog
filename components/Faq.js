a5546cd8b5f71290eb8698fe8136cf0a1a8fb257
Wolfpack <piafyoyo06@gmail.com>
2022-11-25 03:56:26 +0800

    feat: 修改單引號

--- components/Faq.js
+++ components/Faq.js
@@ -36,13 +36,13 @@ export default function Faq() {
               aria-labelledby="headingOne"
               data-bs-parent="#tindogFaqAccordion">
               <div class="accordion-body">
-                <strong>This is the first item's accordion body.</strong> It is
-                shown by default, until the collapse plugin adds the appropriate
-                classes that we use to style each element. These classes control
-                the overall appearance, as well as the showing and hiding via
-                CSS transitions. You can modify any of this with custom CSS or
-                overriding our default variables. It's also worth noting that
-                just about any HTML can go within the{" "}
+                <strong>This is the first item&apos;s accordion body.</strong>{" "}
+                It is shown by default, until the collapse plugin adds the
+                appropriate classes that we use to style each element. These
+                classes control the overall appearance, as well as the showing
+                and hiding via CSS transitions. You can modify any of this with
+                custom CSS or overriding our default variables. It&apos;s also
+                worth noting that just about any HTML can go within the{" "}
                 <code>.accordion-body</code>, though the transition does limit
                 overflow.
               </div>
@@ -66,13 +66,13 @@ export default function Faq() {
               aria-labelledby="headingTwo"
               data-bs-parent="#tindogFaqAccordion">
               <div class="accordion-body">
-                <strong>This is the second item's accordion body.</strong> It is
-                hidden by default, until the collapse plugin adds the
+                <strong>This is the second item&apos;s accordion body.</strong>{" "}
+                It is hidden by default, until the collapse plugin adds the
                 appropriate classes that we use to style each element. These
                 classes control the overall appearance, as well as the showing
                 and hiding via CSS transitions. You can modify any of this with
-                custom CSS or overriding our default variables. It's also worth
-                noting that just about any HTML can go within the{" "}
+                custom CSS or overriding our default variables. It&apos;s also
+                worth noting that just about any HTML can go within the{" "}
                 <code>.accordion-body</code>, though the transition does limit
                 overflow.
               </div>
@@ -96,13 +96,13 @@ export default function Faq() {
               aria-labelledby="headingThree"
               data-bs-parent="#tindogFaqAccordion">
               <div class="accordion-body">
-                <strong>This is the third item's accordion body.</strong> It is
-                hidden by default, until the collapse plugin adds the
+                <strong>This is the third item&apos;s accordion body.</strong>{" "}
+                It is hidden by default, until the collapse plugin adds the
                 appropriate classes that we use to style each element. These
                 classes control the overall appearance, as well as the showing
                 and hiding via CSS transitions. You can modify any of this with
-                custom CSS or overriding our default variables. It's also worth
-                noting that just about any HTML can go within the{" "}
+                custom CSS or overriding our default variables. It&apos;s also
+                worth noting that just about any HTML can go within the{" "}
                 <code>.accordion-body</code>, though the transition does limit
                 overflow.
               </div>
