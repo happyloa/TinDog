c8ebce1212cf1027fa7132ceb1835615ae6554ef
Wolfpack <piafyoyo06@gmail.com>
2022-11-25 04:07:01 +0800

    feat: 完成頁尾區塊

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,18 @@
+// 引入 Styled Components 套件
+import styled from "styled-components";
+
+const StyledFooter = styled.footer`
+  background-color: #ff4c68;
+  color: #ffffff;
+  padding: 25px 0;
+`;
+
 export default function Footer() {
   return (
-    <footer id="footer">
-      <p>© Copyright TinDog</p>
-    </footer>
+    <StyledFooter id="footer">
+      <div className="container text-center">
+        <p>© Copyright Wolfpack</p>
+      </div>
+    </StyledFooter>
   );
 }
