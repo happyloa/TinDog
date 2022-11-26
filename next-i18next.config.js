9bd3181a5cdc960fe061985b9db4fc871467dc1c
Wolfpack <piafyoyo06@gmail.com>
2022-11-26 09:20:03 +0800

    feat: 新增註解

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,3 +1,4 @@
+// 設定多語系 routing 後綴，並指定預設語言為英文
 module.exports = {
   i18n: {
     locales: ["en", "zh-TW"],
--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+// 引入多語系 routing 設定
 const { i18n } = require("./next-i18next.config");
 
 /** @type {import('next').NextConfig} */
